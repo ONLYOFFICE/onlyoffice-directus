2f8bbaede44750711dc90044ea76abfbe4270790
Andrei Panov <andrew.panov@onlyoffice.com>
2024-12-16 12:15:07 +0300

    feat: file save

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,24 +1,36 @@
 import { defineEndpoint } from "@directus/extensions-sdk";
-import editorTemplate from "../assets/templates/editor";
-import { SettingsService } from "../services/settings_service";
+import getEditorTemplate from "../assets/templates/editor";
+import { SettingsService as OnlyofficeSettingsService } from "../services/settings_service";
+import { EditorConfigService, EditorActionType, EditorType } from "../services/editor_config_service";
+import { toArray } from "@directus/utils";
+import { useEnv } from '@directus/env';
+import { getAccountability } from "../utils/get_accountability";
+
+const env = useEnv();
 
 export default defineEndpoint((router, context) => {
-	const { services, getSchema } = context;
-	const { FilesService, CollectionsService } = services;
+	const { services } = context;
+	const { AssetsService, FilesService } = services;
 
-	router.get("/", async (request, response) => {
-		const settingsService = new SettingsService(request, context);
-		
-		response.json(await settingsService.getSettings());
-	});
+	router.get("/editor/:file_id", async (request, response) => {
+		response.setHeader("Content-Security-Policy", `default-src: 'self'`);
 
-	router.get("/editor", async (request, response) => {
-		response.setHeader("Content-Security-Policy", "default-src: *"); // ToDo: strict CSP
+		const settingsService = new OnlyofficeSettingsService(request, context);
+		const editorConfig = new EditorConfigService(request, context);
 
-		response.send(editorTemplate);
+		try {
+			const config = await editorConfig.getConfig(request.params.file_id, request.query.edit ? EditorActionType.Edit : EditorActionType.View, request.query.embed ? EditorType.Embedded : EditorType.Desktop);
+			response.send(getEditorTemplate(config, await settingsService.getSettings()));
+		} catch (error) {
+			request.log.error(error);
+			response.json({ "error": 1, "message": error.message });
+		}
 	});
 
 	router.get("/file/:file_id", async (request, response) => {
+
+		// ToDo: JWT
+
 		// if (req.accountability?.user == null) { 
 		// 	res.status(403); 
 		// 	return response.send(`You don"t have permission to access this.`); 
@@ -27,25 +39,77 @@ export default defineEndpoint((router, context) => {
 		// # proxy https://docs.directus.io/extensions/app-composables.html#useapi
 
 		try {
-			const filesService = new FilesService({
-				schema: await getSchema(),
-				accountability: request.accountability
-			  });
-	
-			const data = await filesService.readOne(request.params.file_id) || null;
+			const service = new AssetsService({
+				schema: request.schema,
+			});
 
-			  console.log("0");
-			  console.log(Object.keys(data));
+			const { stream, file, stat } = await service.getAsset(request.params.file_id, null, null);
+			response.attachment(file.filename_download);
+			response.setHeader("Content-Type", file.type);
+			response.setHeader("Content-Disposition", `attachment; filename="${file.filename_download || file.title}"`);
 
-			if (Buffer.isBuffer(data)) {
-				return response.end(data);
-			} else if (data) {
-				return response.json(data);
-			} else {
-				return response.status(204).end();
-			}
+			stream
+				.on("error", (error: any) => {
+					console.error(error, `Couldn"t stream file ${file.id} to the client`);
+
+					if (!response.headersSent) {
+						response.removeHeader("Content-Type");
+						response.removeHeader("Content-Disposition");
+						response.removeHeader("Cache-Control");
+
+						response.status(500).json({
+							errors: [
+								{
+									message: "An unexpected error occurred.",
+									extensions: {
+										code: "INTERNAL_SERVER_ERROR",
+									},
+								},
+							],
+						});
+					} else {
+						response.end();
+					}
+				})
+				.pipe(response);
 		} catch (error) {
-			response.json({"error": 1, "message": error.message});
+			request.log.error(error);
+			response.json({ "error": 1, "message": error.message });
+		}
+	});
+
+	router.post("/file/:file_id/callback", async (request, response) => {
+
+		// ToDo: JWT
+
+		try {
+			const body = request.body;
+			request.log.info(`ONLYOFFICE Callback POST\n${JSON.stringify(body, null, 2)}`);
+
+			switch (body.status) {
+				case 1: // editing in progress
+					break;
+				case 2: // must save
+				case 3: // corrupted
+					if (body.actions && body.actions.length > 0) {
+						const userId = body.actions[0].userid;
+						request.accountability = await getAccountability(context, userId);
+					}
+					await updateFile(request, request.params.file_id, body.url);
+					break;
+				case 4: // no changes
+					break;
+				case 6: // force save
+				case 7: // force save error
+					break;
+				default:
+					throw new Error(`ONLYOFFICE sent incorrect status ${body.status}`);
+			}
+			response.json({ "error": 0, "message": "success" });
+		} catch (error) {
+			request.log.error(error);
+			response.statusCode = 500;
+			response.json({ "error": 1, "message": error.message },);
 		}
 	});
 
@@ -60,4 +124,16 @@ export default defineEndpoint((router, context) => {
 		}
 	});
 
+	async function updateFile(request: any, fileId: string, fileUrl: string) {
+		const service = new FilesService({
+			schema: request.schema,
+			accountability: request.accountability
+		});
+
+		const response = await fetch(new URL(fileUrl));
+		if (!response.ok) throw new Error(`ONLYOFFICE Couldn't download file: ${response.status} ${response.statusText}`);
+
+		let disk: string = toArray(env['STORAGE_LOCATIONS'] as string)[0]!;
+		await service.uploadOne(response.body, { storage: disk }, fileId);
+	}
 });
